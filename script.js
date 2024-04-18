7133ee2dcc10ac0e7c8384480730d71b8e49b52c
singh-moni <moni.rinki@gmail.com>
2024-04-18 00:36:42 -0400

    Part-2

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-let csvStr = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor's Assistant,26";
+let csvString = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor's Assistant,26";
 
 // This Splits the CSV string into rows spliting whereever it finds('\n')
-const rows = csvStr.split('\n');
+const rows = csvString.split('\n');
 console.log(rows)
 
 
@@ -14,3 +14,27 @@ rows.forEach(row => {
 
 
 
+
+// Extract the header row and split it into an array of column names
+// Split the CSV string into rows
+let Splitcsvrows = csvString.split("\n");
+
+let Data = []
+
+// Extract the header row and split it into an array of column names
+let headers = Splitcsvrows[0].split(',');
+
+
+
+console.log(headers); // Output: ["ID", "Name", "Occupation", "Age"]
+// Iterate through each row starting from the second row (index 1)
+for (let i = 1; i < Splitcsvrows.length; i++) {
+    // Split the row into cells
+    let CellsData = Splitcsvrows[i].split(',');
+
+    // Push each row's cells into the data array
+    Data.push(CellsData);
+    console.log(headers);
+    console.log(CellsData)
+
+}
